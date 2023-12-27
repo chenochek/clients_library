fda4d5e11f5e0d074d6a692ea0b66e5dc4338bb6
Alesia Hancharuk <goncharukalesya@gmail.com>
2023-12-28 00:22:56 +0300

    Change file path for saving

--- backend/utils/saveToFile.js
+++ backend/utils/saveToFile.js
@@ -5,7 +5,7 @@ const saveToFile = (filePath) => {
     try {
         const fileSplitted = filePath.split('/');
         const fileName = fileSplitted[fileSplitted.length - 1]
-        const uploadFolder = path.join(`//public/images/${fileName}`);
+        const uploadFolder = path.join(__dirname, `/public/images/${fileName}`);
         fs.copyFile(filePath, uploadFolder, (error)=> console.log(error))
         return uploadFolder;
     } catch(error) {
